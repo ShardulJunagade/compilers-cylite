90fe6306477896c52554423394a17b8b301640e6
SwayamBorateIIT <borate.swayam@iitgn.ac.in>
2026-04-03 11:11:33 +0000

    assgn3 part2.4

--- cylite_A3/yapl.tab.c
+++ cylite_A3/yapl.tab.c
@@ -143,7 +143,136 @@ static void free_derivation_steps(void);
 #  endif
 # endif
 
-#include "yapl.tab.h"
+
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    ELIF = 258,                    /* ELIF  */
+    PASS = 259,                    /* PASS  */
+    TRY = 260,                     /* TRY  */
+    EXCEPT = 261,                  /* EXCEPT  */
+    PRINT = 262,                   /* PRINT  */
+    RANGE = 263,                   /* RANGE  */
+    IN = 264,                      /* IN  */
+    FOREACH = 265,                 /* FOREACH  */
+    IDENTIFIER = 266,              /* IDENTIFIER  */
+    I_CONSTANT = 267,              /* I_CONSTANT  */
+    F_CONSTANT = 268,              /* F_CONSTANT  */
+    STRING_LITERAL = 269,          /* STRING_LITERAL  */
+    FUNC_NAME = 270,               /* FUNC_NAME  */
+    SIZEOF = 271,                  /* SIZEOF  */
+    PTR_OP = 272,                  /* PTR_OP  */
+    INC_OP = 273,                  /* INC_OP  */
+    DEC_OP = 274,                  /* DEC_OP  */
+    LEFT_OP = 275,                 /* LEFT_OP  */
+    RIGHT_OP = 276,                /* RIGHT_OP  */
+    LE_OP = 277,                   /* LE_OP  */
+    GE_OP = 278,                   /* GE_OP  */
+    EQ_OP = 279,                   /* EQ_OP  */
+    NE_OP = 280,                   /* NE_OP  */
+    TH_OP = 281,                   /* TH_OP  */
+    AND_OP = 282,                  /* AND_OP  */
+    OR_OP = 283,                   /* OR_OP  */
+    MUL_ASSIGN = 284,              /* MUL_ASSIGN  */
+    DIV_ASSIGN = 285,              /* DIV_ASSIGN  */
+    MOD_ASSIGN = 286,              /* MOD_ASSIGN  */
+    ADD_ASSIGN = 287,              /* ADD_ASSIGN  */
+    SUB_ASSIGN = 288,              /* SUB_ASSIGN  */
+    LEFT_ASSIGN = 289,             /* LEFT_ASSIGN  */
+    RIGHT_ASSIGN = 290,            /* RIGHT_ASSIGN  */
+    AND_ASSIGN = 291,              /* AND_ASSIGN  */
+    XOR_ASSIGN = 292,              /* XOR_ASSIGN  */
+    OR_ASSIGN = 293,               /* OR_ASSIGN  */
+    TYPEDEF_NAME = 294,            /* TYPEDEF_NAME  */
+    ENUMERATION_CONSTANT = 295,    /* ENUMERATION_CONSTANT  */
+    TYPEDEF = 296,                 /* TYPEDEF  */
+    EXTERN = 297,                  /* EXTERN  */
+    STATIC = 298,                  /* STATIC  */
+    AUTO = 299,                    /* AUTO  */
+    REGISTER = 300,                /* REGISTER  */
+    INLINE = 301,                  /* INLINE  */
+    CONST = 302,                   /* CONST  */
+    RESTRICT = 303,                /* RESTRICT  */
+    VOLATILE = 304,                /* VOLATILE  */
+    BOOL = 305,                    /* BOOL  */
+    CHAR = 306,                    /* CHAR  */
+    SHORT = 307,                   /* SHORT  */
+    INT = 308,                     /* INT  */
+    LONG = 309,                    /* LONG  */
+    SIGNED = 310,                  /* SIGNED  */
+    UNSIGNED = 311,                /* UNSIGNED  */
+    FLOAT = 312,                   /* FLOAT  */
+    DOUBLE = 313,                  /* DOUBLE  */
+    VOID = 314,                    /* VOID  */
+    COMPLEX = 315,                 /* COMPLEX  */
+    IMAGINARY = 316,               /* IMAGINARY  */
+    STRUCT = 317,                  /* STRUCT  */
+    UNION = 318,                   /* UNION  */
+    ENUM = 319,                    /* ENUM  */
+    ELLIPSIS = 320,                /* ELLIPSIS  */
+    CASE = 321,                    /* CASE  */
+    DEFAULT = 322,                 /* DEFAULT  */
+    IF = 323,                      /* IF  */
+    ELSE = 324,                    /* ELSE  */
+    SWITCH = 325,                  /* SWITCH  */
+    WHILE = 326,                   /* WHILE  */
+    DO = 327,                      /* DO  */
+    FOR = 328,                     /* FOR  */
+    GOTO = 329,                    /* GOTO  */
+    CONTINUE = 330,                /* CONTINUE  */
+    BREAK = 331,                   /* BREAK  */
+    RETURN = 332,                  /* RETURN  */
+    ALIGNAS = 333,                 /* ALIGNAS  */
+    ALIGNOF = 334,                 /* ALIGNOF  */
+    ATOMIC = 335,                  /* ATOMIC  */
+    GENERIC = 336,                 /* GENERIC  */
+    NORETURN = 337,                /* NORETURN  */
+    STATIC_ASSERT = 338,           /* STATIC_ASSERT  */
+    THREAD_LOCAL = 339,            /* THREAD_LOCAL  */
+    LOWER_THAN_ELSE = 340          /* LOWER_THAN_ELSE  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 87 "yapl.y"
+
+	int val;
+	struct symtab *symp;
+
+#line 261 "yapl.tab.c"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+
 /* Symbol kind.  */
 enum yysymbol_kind_t
 {
@@ -2137,31 +2266,31 @@ yyreduce:
   case 7: /* constant: I_CONSTANT  */
 #line 103 "yapl.y"
                      {int_consts++;}
-#line 2141 "yapl.tab.c"
+#line 2270 "yapl.tab.c"
     break;
 
   case 167: /* declarator: pointer direct_declarator  */
 #line 400 "yapl.y"
                                     {pointer_decls++;}
-#line 2147 "yapl.tab.c"
+#line 2276 "yapl.tab.c"
     break;
 
   case 257: /* elif_list: ELIF '(' expression ')' statement  */
 #line 563 "yapl.y"
                                             { ladder_len++; }
-#line 2153 "yapl.tab.c"
+#line 2282 "yapl.tab.c"
     break;
 
   case 258: /* elif_list: elif_list ELIF '(' expression ')' statement  */
 #line 564 "yapl.y"
                                                       { ladder_len++; }
-#line 2159 "yapl.tab.c"
+#line 2288 "yapl.tab.c"
     break;
 
   case 259: /* $@1: %empty  */
 #line 568 "yapl.y"
                                            { ladder_len++; (yyvsp[0].val) = (ladder_len-1); }
-#line 2165 "yapl.tab.c"
+#line 2294 "yapl.tab.c"
     break;
 
   case 260: /* selection_statement: IF '(' expression ')' statement ELSE $@1 statement  */
@@ -2170,19 +2299,19 @@ yyreduce:
             if(ladder_len >= max) { max = ladder_len; } 
             ladder_len = (yyvsp[-2].val); 
         }
-#line 2174 "yapl.tab.c"
+#line 2303 "yapl.tab.c"
     break;
 
   case 261: /* selection_statement: IF '(' expression ')' statement  */
 #line 574 "yapl.y"
         { ifs_wo_else++; }
-#line 2180 "yapl.tab.c"
+#line 2309 "yapl.tab.c"
     break;
 
   case 262: /* $@2: %empty  */
 #line 576 "yapl.y"
         { ladder_len++; (yyvsp[0].val) = (ladder_len-1); }
-#line 2186 "yapl.tab.c"
+#line 2315 "yapl.tab.c"
     break;
 
   case 263: /* selection_statement: IF '(' expression ')' statement elif_list ELSE $@2 statement  */
@@ -2191,35 +2320,35 @@ yyreduce:
             if(ladder_len >= max) { max = ladder_len; } 
             ladder_len = (yyvsp[-2].val); 
         }
-#line 2195 "yapl.tab.c"
+#line 2324 "yapl.tab.c"
     break;
 
   case 264: /* selection_statement: IF '(' expression ')' statement elif_list  */
 #line 583 "yapl.y"
         { /* Logic for if-elif without final else */ }
-#line 2201 "yapl.tab.c"
+#line 2330 "yapl.tab.c"
     break;
 
   case 279: /* translation_unit: external_declaration  */
 #line 608 "yapl.y"
                                {global_declarations++;}
-#line 2207 "yapl.tab.c"
+#line 2336 "yapl.tab.c"
     break;
 
   case 280: /* translation_unit: translation_unit external_declaration  */
 #line 609 "yapl.y"
                                                 {global_declarations++;}
-#line 2213 "yapl.tab.c"
+#line 2342 "yapl.tab.c"
     break;
 
   case 281: /* external_declaration: function_definition  */
 #line 613 "yapl.y"
                               {func_definitions++;}
-#line 2219 "yapl.tab.c"
+#line 2348 "yapl.tab.c"
     break;
 
 
-#line 2223 "yapl.tab.c"
+#line 2352 "yapl.tab.c"
 
       default: break;
     }
